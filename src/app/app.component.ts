6091b1b979800c84993a980905440de221170681
gustavoesteves <gustavoestevesr@directon.com.br>
2024-07-25 11:11:01 -0300

    feat: inclusão de toolbar com searbar e botão de checkout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+  productInCheckout = 0;
+  searchbarInput = ''
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  searchProduct() {
+    if (this.searchbarInput.length > 0) {
+      this.updateQueryParams(this.searchbarInput);
+    } else {
+      this.resetQueryParams()
+    }
+  }
+
+  goToProductDetails(productId: string) {
+    this.router.navigate(['/productDetails', productId]);
+  }
+
+  updateQueryParams(productId: string) {
+    // Para adicionar ou atualizar um único parâmetro
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { product: productId },
+      queryParamsHandling: 'merge'
+    });
+  }
+
+  resetQueryParams() {
+    // Para redefinir todos os parâmetros de consulta
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {},
+      queryParamsHandling: ''
+    });
+  }
+}
