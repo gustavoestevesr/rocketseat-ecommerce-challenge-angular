63e93d970060fb45a7d6d5c6714036baf0c6a128
gustavoesteves <gustavoestevesr@directon.com.br>
2024-07-25 23:39:35 -0300

    feat: novas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: ProductListComponent
+  },
+  {
+    path: 'productDetails/:productId', component: ProductDetailsComponent
+  },
+  {
+    path: 'checkout', component: CheckoutComponent
+  },
+];
