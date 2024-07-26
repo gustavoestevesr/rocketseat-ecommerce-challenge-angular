b994f24f0b55fa76c0ab4b7bdd14e148e930bb7e
gustavoesteves <gustavoestevesr@directon.com.br>
2024-07-25 23:40:27 -0300

    feat: novos enums

---
+++ src/app/enums/categories.enum.ts
@@ -0,0 +1,5 @@
+export enum EnumCategories {
+  TODOS_OS_PRODUTOS = 'TODOS OS PRODUTOS',
+  CAMISETAS = 'CAMISETAS',
+  CANECAS = 'CANECAS'
+}
---
+++ src/app/enums/orderByOptions.enum.ts
@@ -0,0 +1,6 @@
+export enum EnumOrderByOptions {
+  NOVIDADES = 'Novidades',
+  PRECO_MAIOR_MENOR = 'Preço: maior - menor',
+  PRECO_MENOR_MAIOR = 'Preço: menor - maior',
+  MAIS_VENDIDOS = 'Mais vendidos',
+}
