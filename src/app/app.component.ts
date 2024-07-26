048836c76f004bb4c68d6c0103a1f4e3f5b021ff
gustavoesteves <gustavoestevesr@directon.com.br>
2024-07-25 23:41:13 -0300

    feat: observando parametros da rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [CommonModule, RouterOutlet, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rocketseat-challenge-angular';
+  productFiltered = ''
+
+  route = inject(ActivatedRoute)
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params: any) => {
+      const product = params.get('product');
+      console.log('Query Parameter:', product);
+    });
+  }
 }
