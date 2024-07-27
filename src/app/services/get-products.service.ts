288e598f8a4445bc6fb952c20b6ba44edf6dacd8
Gustavo Esteves <gustavo.tech4me@gmail.com>
2024-07-27 13:22:18 -0300

    feat: serviço para obter, cachear e observar mudanças do fake db de produtos

--- src/app/services/get-products.service.ts
+++ src/app/services/get-products.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Product } from '../models/product.model';
+import { RESPONSE } from '../fake-db/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetProductsService {
 
-  constructor() { }
+  allProducts: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+  productsCache$: Observable<Product[]> = this.allProducts.asObservable();
+
+  constructor() {
+    this.allProducts.next(RESPONSE.products);
+  }
+
+  getAll(): Observable<Product[]> {
+    return this.productsCache$;
+  }
 }
