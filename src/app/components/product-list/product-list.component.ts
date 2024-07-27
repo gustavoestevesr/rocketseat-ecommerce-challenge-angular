0921ff52dff6d54802cef1fb7b864fa2ac40529c
Gustavo Esteves <gustavo.tech4me@gmail.com>
2024-07-27 13:21:07 -0300

    feat: listagem de produtos com uso de serviço com getAll

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { EnumCategories } from '../../enums/categories.enum';
 import { CommonModule } from '@angular/common';
 import { EnumOrderByOptions } from '../../enums/orderByOptions.enum';
 import { FormsModule } from '@angular/forms';
+import { ProductCardComponent } from "./product-card/product-card.component";
+import { Product } from '../../models/product.model';
+import { GetProductsService } from '../../services/get-products.service';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ProductCardComponent, ProductCardComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
@@ -16,6 +19,14 @@ export class ProductListComponent {
   orderByOptions = [EnumOrderByOptions.NOVIDADES, EnumOrderByOptions.PRECO_MAIOR_MENOR, EnumOrderByOptions.PRECO_MENOR_MAIOR, EnumOrderByOptions.MAIS_VENDIDOS]
   activedCategory = EnumCategories.TODOS_OS_PRODUTOS;
   orderBySelected = null;
+
+  productsList: Product[] = [];
+
+  getProductsService = inject(GetProductsService);
+
+  constructor() {
+    this.getProductsService.getAll().subscribe((data) => this.productsList = data);
+  }
 
   changeCategory(categorySelected: EnumCategories) {
     this.activedCategory = categorySelected;
