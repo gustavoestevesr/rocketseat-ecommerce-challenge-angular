be107a4c1b502e60125f6bc46cf5712b3457447a
Gustavo Esteves <gustavo.tech4me@gmail.com>
2024-07-26 21:59:16 -0300

    feat: filtro de ordenação

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
 import { EnumCategories } from '../../enums/categories.enum';
 import { CommonModule } from '@angular/common';
+import { EnumOrderByOptions } from '../../enums/orderByOptions.enum';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
   categories = [EnumCategories.TODOS_OS_PRODUTOS, EnumCategories.CAMISETAS, EnumCategories.CANECAS]
-  orderByOptions = []
+  orderByOptions = [EnumOrderByOptions.NOVIDADES, EnumOrderByOptions.PRECO_MAIOR_MENOR, EnumOrderByOptions.PRECO_MENOR_MAIOR, EnumOrderByOptions.MAIS_VENDIDOS]
   activedCategory = EnumCategories.TODOS_OS_PRODUTOS;
+  orderBySelected = null;
 
-  activateCategory(categorySelected: EnumCategories) {
+  changeCategory(categorySelected: EnumCategories) {
     this.activedCategory = categorySelected;
   }
 
   isActive(categorySelected: EnumCategories) {
     return this.activedCategory === categorySelected;
   }
+
+  changeOrderBy() {
+    console.log('Order by selected:', this.orderBySelected);
+  }
+
+
 }
