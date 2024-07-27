8dfb8be9be7de3786994956f636e16e87200e42a
Gustavo Esteves <gustavo.tech4me@gmail.com>
2024-07-27 13:21:47 -0300

    feat: fake db de produtos

---
+++ src/app/fake-db/products.ts
@@ -0,0 +1,80 @@
+import { Product } from "../models/product.model";
+
+const PRODUCT_LIST: Product[] = [
+  {
+    image: 'https://cdn.pixabay.com/photo/2020/04/25/08/44/mug-5089934_1280.jpg',
+    title: 'Caneca de porselanato',
+    price: 220,
+    quantity: 2,
+    enable: true,
+    onSale: false,
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2020/04/25/08/44/mug-5089934_1280.jpg',
+    title: 'Caneca de porselanato',
+    price: 220,
+    quantity: 2,
+    enable: true,
+    onSale: false,
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2020/04/25/08/44/mug-5089934_1280.jpg',
+    title: 'Caneca de porselanato',
+    price: 220,
+    quantity: 2,
+    enable: true,
+    onSale: false,
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2020/04/25/08/44/mug-5089934_1280.jpg',
+    title: 'Caneca de porselanato',
+    price: 220,
+    quantity: 2,
+    enable: true,
+    onSale: false,
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2020/04/25/08/44/mug-5089934_1280.jpg',
+    title: 'Caneca de porselanato',
+    price: 220,
+    quantity: 2,
+    enable: true,
+    onSale: false,
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2020/04/25/08/44/mug-5089934_1280.jpg',
+    title: 'Caneca de porselanato',
+    price: 220,
+    quantity: 2,
+    enable: true,
+    onSale: false,
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2020/04/25/08/44/mug-5089934_1280.jpg',
+    title: 'Caneca de porselanato',
+    price: 220,
+    quantity: 2,
+    enable: true,
+    onSale: false,
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2020/04/25/08/44/mug-5089934_1280.jpg',
+    title: 'Caneca de porselanato',
+    price: 220,
+    quantity: 2,
+    enable: true,
+    onSale: false,
+  },
+  {
+    image: 'https://cdn.pixabay.com/photo/2020/04/25/08/44/mug-5089934_1280.jpg',
+    title: 'Caneca de porselanato',
+    price: 220,
+    quantity: 2,
+    enable: true,
+    onSale: false,
+  }
+]
+
+export const RESPONSE = {
+  products: PRODUCT_LIST,
+}
