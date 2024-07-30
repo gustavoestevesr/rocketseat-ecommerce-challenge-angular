3b626d90cae1bed809c5a111769622137c339688
Gustavo Esteves <gustavo.tech4me@gmail.com>
2024-07-29 23:09:27 -0300

    feat: filtragem de produtos e busca de produto específico

--- src/app/services/get-products.service.ts
+++ src/app/services/get-products.service.ts
@@ -7,15 +7,32 @@ import { RESPONSE } from '../fake-db/products';
   providedIn: 'root'
 })
 export class GetProductsService {
-
   allProducts: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
   productsCache$: Observable<Product[]> = this.allProducts.asObservable();
+  filteredProducts: Product[] = [];
 
   constructor() {
+    this.loadProducts()
+  }
+
+  loadProducts() {
     this.allProducts.next(RESPONSE.products);
+    this.filteredProducts = RESPONSE.products;
   }
 
   getAll(): Observable<Product[]> {
     return this.productsCache$;
+  }
+
+  getOne(productId: string) {
+    return this.allProducts.getValue().find((p) => p.id === productId) || null
+  }
+
+  setFilteredList(newProducts: Product[]) {
+    this.filteredProducts = newProducts;
+  }
+
+  getFilteredList(): Product[] {
+    return this.filteredProducts;
   }
 }
