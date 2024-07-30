f5e707c38da281a3d852657b2b71ae44b976d42e
Gustavo Esteves <gustavo.tech4me@gmail.com>
2024-07-29 23:08:46 -0300

    feat: apresentando objeto selecionado com mais detalhes a partir da rota

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { GetProductsService } from '../../services/get-products.service';
+import { Product } from '../../models/product.model';
+import { ActivatedRoute } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  productSelected!: Product | null;
 
+  productService = inject(GetProductsService)
+  route = inject(ActivatedRoute)
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const productId = params.get('productId');
+      if (productId) {
+        this.productSelected = this.productService.getOne(productId);
+      }
+    });
+  }
 }
