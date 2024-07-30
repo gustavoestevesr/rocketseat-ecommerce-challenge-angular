34dbec6638c2b6ffee226f38d55a4145b2be9be7
Gustavo Esteves <gustavo.tech4me@gmail.com>
2024-07-29 23:09:46 -0300

    feat: método inicial para filtragem de produtos

--- src/app/services/filter-products.service.ts
+++ src/app/services/filter-products.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { EnumCategories } from '../enums/categories.enum';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class FilterProductsService {
 
   constructor() { }
+
+  filterProducts(productsList: Product[], category: EnumCategories): Product[] {
+    return productsList;
+  }
 }
