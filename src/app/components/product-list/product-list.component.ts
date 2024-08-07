4c2e39a0fcf0cce32aa3aa36838f5362de152373
Gustavo Esteves <gustavo.tech4me@gmail.com>
2024-08-06 22:17:13 -0300

    refactor: refatoração de melhoria

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,44 +1,120 @@
-import { Component, inject } from '@angular/core';
-import { EnumCategories } from '../../enums/categories.enum';
 import { CommonModule } from '@angular/common';
-import { EnumOrderByOptions } from '../../enums/orderByOptions.enum';
-import { FormsModule } from '@angular/forms';
-import { ProductCardComponent } from "./product-card/product-card.component";
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Product } from '../../models/product.model';
-import { GetProductsService } from '../../services/get-products.service';
+import { ProductFilterService } from '../../services/filter-products.service';
+import { ProductsService } from '../../services/products.service';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { EnumCategories } from '../../enums/categories.enum';
+import { EnumOrderByOptions } from '../../enums/orderByOptions.enum';
+import { debounceTime, distinctUntilChanged, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, FormsModule, ProductCardComponent, ProductCardComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ProductCardComponent,
+    ProductCardComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
-export class ProductListComponent {
-  categories = [EnumCategories.TODOS_OS_PRODUTOS, EnumCategories.CAMISETAS, EnumCategories.CANECAS]
-  orderByOptions = [EnumOrderByOptions.NOVIDADES, EnumOrderByOptions.PRECO_MAIOR_MENOR, EnumOrderByOptions.PRECO_MENOR_MAIOR, EnumOrderByOptions.MAIS_VENDIDOS]
-  activedCategory = EnumCategories.TODOS_OS_PRODUTOS;
-  orderBySelected = null;
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
 
-  productsList: Product[] = [];
+  activeCategory = EnumCategories.TODOS_OS_PRODUTOS;
+  categories = [
+    EnumCategories.TODOS_OS_PRODUTOS,
+    EnumCategories.CAMISETA,
+    EnumCategories.CANECA,
+  ];
 
-  getProductsService = inject(GetProductsService);
+  orderByFormControl = new FormControl(null);
+  sortOrder: 'asc' | 'desc' = 'asc';
+  orderByOptions = [
+    EnumOrderByOptions.NOVIDADES,
+    EnumOrderByOptions.PRECO_MAIOR_MENOR,
+    EnumOrderByOptions.PRECO_MENOR_MAIOR,
+    EnumOrderByOptions.MAIS_VENDIDOS,
+  ];
+  sortProperty: keyof Product = 'title';
 
-  constructor() {
-    this.productsList = this.getProductsService.getFilteredList();
+  productService = inject(ProductsService);
+  productFilterService = inject(ProductFilterService);
+
+  ngOnInit(): void {
+    this.loadProducts();
+    this.onValueChangeOrderBy$();
   }
 
-  changeCategory(categorySelected: EnumCategories) {
-    this.activedCategory = categorySelected;
+  loadProducts(): void {
+    this.productService.getProducts().subscribe((data: Product[]) => {
+      this.products = data;
+    });
+  }
+
+  onValueChangeOrderBy$() {
+    this.orderByFormControl.valueChanges
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(100),
+        startWith(EnumOrderByOptions.NOVIDADES)
+      )
+      .subscribe((type) => {
+        this.adjustParmsOrderByToApplyFilter(type || EnumOrderByOptions.NOVIDADES);
+        this.applyFilters();
+      });
+  }
+
+  adjustParmsOrderByToApplyFilter(
+    type: EnumOrderByOptions
+  ) {
+    switch (type) {
+      case EnumOrderByOptions.MAIS_VENDIDOS:
+        this.sortProperty = 'qtdSold';
+        this.sortOrder = 'desc';
+        return;
+      case EnumOrderByOptions.NOVIDADES:
+        this.sortProperty = 'createAt';
+        this.sortOrder = 'desc';
+        return;
+      case EnumOrderByOptions.PRECO_MAIOR_MENOR:
+        this.sortProperty = 'price';
+        this.sortOrder = 'desc';
+        return;
+      case EnumOrderByOptions.PRECO_MENOR_MAIOR:
+        this.sortProperty = 'price';
+        this.sortOrder = 'asc';
+        return;
+    }
+  }
+
+  applyFilters(): void {
+    let result = this.products;
+    if (this.activeCategory) {
+      result = this.productFilterService.filterByCategory(
+        result,
+        this.activeCategory
+      );
+    }
+    result = this.productFilterService.sortBy(
+      result,
+      this.sortProperty,
+      this.sortOrder
+    );
+    this.filteredProducts = result;
+  }
+
+  onCategoryChange(category: EnumCategories): void {
+    this.activeCategory = category;
+    this.applyFilters();
   }
 
   isActive(categorySelected: EnumCategories) {
-    return this.activedCategory === categorySelected;
+    return this.activeCategory === categorySelected;
   }
-
-  changeOrderBy() {
-    console.log('Order by selected:', this.orderBySelected);
-  }
-
-
 }
